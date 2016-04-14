3c5707bb441064275afc9f38e38070825a560ed2
rolledback <matthew.rayermann@gmail.com>
2016-04-14 00:29:37 -0500

    Beginning to simulate stock values. Updated build script.

--- src/Stock.java
+++ src/Stock.java
@@ -1,15 +1,80 @@
+import java.io.*;
+import java.util.Random;
+
 public class Stock {
+
+    public static void main(String[] args) {
+        Stock test = new Stock(Util.genRandomId(), 100, 50, 500);
+        test.dumpToPriceHistoryToFile("price");
+    }
 
     public int numAvailable;
     public String id;
+    public int maxCycles;
+
+    private double startingPrice;
+    private double volatility;
+    private double drift;
+    private double[] priceHistory;
+
+    private static final double timeStep = 0.01;
+    public static final Random rand = new Random();
 
     public Stock(String id, int numAvailable) {
         this.id = id;
         this.numAvailable = numAvailable;
     }
 
+    public Stock(String id, int numAvailable, int startingPrice, int maxCycles) {
+        this.id = id;
+        this.numAvailable = numAvailable;
+        this.maxCycles = maxCycles;
+        this.startingPrice = startingPrice;
+
+        this.priceHistory = new double[maxCycles];
+
+        genRandomParams();
+        fillPriceHistory();
+    }
+
+    public void dumpToPriceHistoryToFile(String file) {
+        try {
+            FileWriter fstream = new FileWriter(file + ".stock");
+            BufferedWriter out = new BufferedWriter(fstream);
+
+            for(int i = 0; i < maxCycles; i++) {
+                out.write(Double.toString(priceHistory[i]) + "\n");
+            }
+
+            out.close();
+        }
+        catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void genRandomParams() {
+        // TODO: these need to be randomly generated
+        volatility = 1;
+        drift = 0.1;
+    }
+
+    private void fillPriceHistory() {
+        // see here for math explaination: http://investexcel.net/geometric-brownian-motion-excel/
+        double delta, totalDrift, uncertainty;
+        priceHistory[0] = startingPrice;
+        for(int i = 1; i < maxCycles; i++) {
+            totalDrift = (priceHistory[i - 1] * drift * timeStep);
+            uncertainty=  (priceHistory[i - 1] * volatility * rand.nextGaussian() * Math.sqrt(timeStep));
+            delta = totalDrift + uncertainty;
+
+            priceHistory[i] = priceHistory[i - 1] + delta;
+        }
+    }
+
     public int getValue(int tick) {
-        return 1;
+        // TODO: this needs to return double, lots of refactoring tbd to make that work
+        return (int)priceHistory[tick];
     }
 
     public void decrementAvailable(int quantity) {
--- src/build.ps1
+++ src/build.ps1
@@ -1,21 +1,63 @@
-$javaCompile = "& javac *.java 2>&1";
-echo "Compiling..."
-$compileOutput = invoke-expression $javaCompile;
+param(
+    [switch] $console,
+    [switch] $serverTest,
+    [switch] $stockTest,
+    [switch] $compile
+)
 
-If ($compileOutput.length -eq 0)
+If ($compile)
 {
+    $javaCompile = "& javac *.java 2>&1";
+    echo "Compiling..."
+    invoke-expression $javaCompile;
+}
+ElseIf ($serverTest)
+{
+    $javaCompile = "& javac *.java 2>&1";
+    echo "Compiling..."
+    $compileOutput = invoke-expression $javaCompile;
     echo "Compilation complete."
-    start-process powershell.exe -argument '-noexit -nologo -noprofile -executionpolicy bypass -command java StockTrader"'
-    sleep(1)
-    start-process powershell.exe -argument '-noexit -nologo -noprofile -executionpolicy bypass -command java HumanClient"'
-    start-process powershell.exe -argument '-noexit -nologo -noprofile -executionpolicy bypass -command java ConsoleClient"'
-}
-Else
-{
-    echo "Compilation failed."
-    for ($i=0; $i -lt $compileOutput.length; $i++)
-    {
-        $compileOutput[$i];
-    }
-}
 
+    If ($compileOutput.length -eq 0)
+    {
+        echo "Starting server."
+        start-process powershell.exe -argument '-noexit -nologo -noprofile -executionpolicy bypass -command java StockTrader"'
+        sleep(1)
+        echo "Starting human client."
+        start-process powershell.exe -argument '-noexit -nologo -noprofile -executionpolicy bypass -command java HumanClient"'
+        If ($console)
+        {
+            echo "Starting console."
+            start-process powershell.exe -argument '-noexit -nologo -noprofile -executionpolicy bypass -command java ConsoleClient"'
+        }
+    }
+    Else
+    {
+        echo "Compilation failed."
+        for ($i=0; $i -lt $compileOutput.length; $i++)
+        {
+            $compileOutput[$i];
+        }
+    }    
+}
+ElseIf ($stockTest)
+{
+    $javaCompile = "& javac *.java 2>&1";
+    echo "Compiling..."
+    $compileOutput = invoke-expression $javaCompile;
+    echo "Compilation complete."
+
+    If ($compileOutput.length -eq 0)
+    {
+        echo "Running stock test."
+        java Stock
+    }
+    Else
+    {
+        echo "Compilation failed."
+        for ($i=0; $i -lt $compileOutput.length; $i++)
+        {
+            $compileOutput[$i];
+        }
+    } 
+}
