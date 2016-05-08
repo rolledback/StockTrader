ae0c101a9f8e240db3eb2cd924094e379cea63a1
rolledback <matthew.rayermann@gmail.com>
2016-05-08 15:30:36 -0500

    Going with Java's DocumentBuilder.

--- src/ScenarioReader.java
+++ src/ScenarioReader.java
@@ -1,4 +1,27 @@
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import java.io.*;
+
 public class ScenarioReader {
     public static void main(String[] args) {
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse("../scenarios/example_scenario.xml");
+        }
+        catch(ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        catch(SAXException e) {
+            e.printStackTrace();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
