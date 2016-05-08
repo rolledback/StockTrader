261a2fcfbec9ebdda77192f3fc2074d5f9ff21b1
rolledback <matthew.rayermann@gmail.com>
2016-05-07 19:45:56 -0500

    Not going to use JAXB anymore. Back to square one on figuring out what xml parser to use.

--- src/ScenarioReader.java
+++ src/ScenarioReader.java
@@ -1,22 +1,4 @@
-import java.io.*;
-import javax.xml.bind.annotation.*;
-
 public class ScenarioReader {
     public static void main(String[] args) {
-        RootElementClass adr = new RootElementClass();
-        FileInputStream adrFile = null;
-        String scenarioFile = args[0];
-        try {
-            adrFile = new FileInputStream(scenarioFile);
-            JAXBContext ctx = JAXBContext.newInstance(RootElementClass.class);
-            Unmarshaller um = ctx.createUnmarshaller();
-            adr = (RootElementClass) um.unmarshal(adrFile);
-        }
-        catch(IOException exc) {
-        }
-        catch(JAXBException exc) {
-        }
-        finally {
-        }
     }
 }
